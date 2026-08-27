e8c5c80b67d279626de35075f02b26cba72fe9f1
Vikas Jangid <vikasjangid3352@gmail.com>
2026-08-27 12:18:14 +0530

    chore: remove entire shinelimos project and backend repository

--- shinelimos/backend/src/routes/dashboard.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const dashboardController = require("../controllers/dashboard");
-
-// Get dashboard data
-router.get("/dashboard", dashboardController.get_dashboardData);
-
-module.exports = router;
